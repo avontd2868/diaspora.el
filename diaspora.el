5100ba61aa5e95bfd1e6d15929b1685134e6eea8
cnngimenez <cnngimenez@gmail.com>
2012-01-22 18:44:33 -0300

    Inserted function diaspora-show-message-new-buffer.
    This function will work as interactive, so user can clic on a message to see all comments and the message in a new buffer.
    
    diaspora-get-single-message added so diaspora.el can get a message throught its id.
    
    diaspora-parse-single-message-json added so it can parse a single message showing all comments.

--- diaspora.el
+++ diaspora.el
@@ -77,6 +77,10 @@ If nil, you will be prompted."
   "URL used to update diaspora status messages."
   :group 'diaspora)
 
+(defcustom diaspora-single-message-url
+  "https://joindiaspora.com/posts"
+  "URL used to get a single message.")
+
 (defcustom diaspora-entry-stream-url 
   "https://joindiaspora.com/stream.json"
   "JSON version of the entry stream(the main stream)."
@@ -125,6 +129,9 @@ If nil, you will be prompted."
 (defvar diaspora-post-buffer "*diaspora post*"
   "The name of the diaspora post buffer.")
 
+(defvar diaspora-single-message-buffer "*diaspora message*"
+  "The name of the diaspora single message buffer."
+  )
 
 ;;; User Functions:
 
@@ -243,6 +250,13 @@ if not, the buffer called \"Diáspora Stream\" will be re-used or created if nee
 	   ("Accept-Charset" . "UTF-8"))))
     (url-retrieve-synchronously url)))
 
+(defun diaspora-delete-http-header ()
+  "Delete the first lines that is the HTTP header in the current buffer.
+This is used after getting a stream or any URL in JSON format."
+   (goto-char (point-min))
+   (search-forward "\n\n")      
+   (delete-region (point-min) (match-beginning 0))
+  )
 
 (defun diaspora-get-entry-stream ()
   "Show the entry stream. 
@@ -251,20 +265,25 @@ I expect to be already logged in. Use `diaspora' for log-in."
   (interactive)  
   (diaspora-ask) ;; don't forget username and password!
   (diaspora-authenticity-token diaspora-sign-in-url) ;; Get the authenticity token
+  ;; get the in JSON format all the data
   (let ((buff (diaspora-get-url-entry-stream diaspora-entry-stream-url)))
     (with-current-buffer buff
       ;; Delete the HTTP header...
-      (goto-char (point-min))
-      (search-forward "\n\n")      
-      (delete-region (point-min) (match-beginning 0))
+      (diaspora-delete-http-header)
       ;; Parse JSON...
       (diaspora-parse-json)
 
-      ;;Change markdown to html
-      (diaspora-change-to-html)
+      ;;Change markdown to html... not so good.      
+      ;;(diaspora-change-to-html)
+      ;;Better using diaspora-mode already done by Tiago!      
+      (diaspora-mode) 
+      (setq (make-local-variable buffer-read-only) t)
+
+      (goto-char (point-min))
       )
     ;; Delete HTTP Buffer
-    (kill-buffer buff)))
+    ;;(kill-buffer buff)
+    ))
 
 
 (defun diaspora-get-tmp-path (filename)
@@ -282,28 +301,80 @@ Check if the temporal directory exists, if not create it."
   (markdown-preview)
   )
 
-(defun diaspora-show-message (parsed-message &optional buffer)
-  "Show a parsed message in a given buffer."
-  (with-current-buffer buffer
-    (let ( (name (cdr (assoc 'name (assoc 'author parsed-message))))
-	  (diaspora_id (cdr (assoc 'diaspora_id (assoc 'author parsed-message))))
-	  (text (cdr (assoc 'text parsed-message)))
-	  (date (cdr (assoc 'created_at parsed-message)))
-	  (amount-comments (cdr (assoc 'comments_count parsed-message)))
-	  (amount-likes (cdr (assoc 'likes_count parsed-message)))
-	  ;; We can look for more data, including the last 3 comments!
-	  )
-      (insert  "---\n")
-      (insert (propertize
-	       (format "%s(%s):\n" name diaspora_id)
-	       'mouse-face 'highlight
-	       'face "link"
-	       'keymap 'diaspora-show-message-map
-	       'help-echo "Click here to see this message in new buffer."))
-      (insert (format "%s\n" date))
-      (insert (format "Has %s comments. %s likes.\n" amount-comments amount-likes))
-      (insert (format "%s\n\n" text)))))
+(defvar diaspora-show-message-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "\r" 'diaspora-show-message-new-buffer)
+    (define-key map [mouse-2] 'diaspora-show-message-new-buffer)
+    map)
+  "Keymap used when the user clics on a name link.")
 
+(defun diaspora-show-message (parsed-message &optional buffer)
+  "Show a parsed message in a given buffer.
+If buffer is nil, then use the `current-buffer'."
+  ;; Ensure that buffer is not nil, in case is nil, buffer will be `current-buffer'.
+  (let ((buffer (unless buffer
+		  (current-buffer))))
+    (with-current-buffer buffer
+      (let ( 
+	    (id (cdr (assoc 'id parsed-message)))
+	    (name (cdr (assoc 'name (assoc 'author parsed-message))))
+	    (diaspora_id (cdr (assoc 'diaspora_id (assoc 'author parsed-message))))
+	    (text (cdr (assoc 'text parsed-message)))
+	    (date (cdr (assoc 'created_at parsed-message)))
+	    (amount-comments (cdr (assoc 'comments_count parsed-message)))
+	    (amount-likes (cdr (assoc 'likes_count parsed-message)))
+	    ;; We can look for more data, including the last 3 comments!
+	    )
+	(insert  "---\n")
+	(insert (propertize
+		 (format "%s(%s):\n" name diaspora_id)
+		 'mouse-face 'highlight
+		 'face "link"
+		 'keymap 'diaspora-show-message-map
+		 'diaspora-id-message id
+		 'help-echo "Click here to see this message in new buffer."))
+	(insert (format "%s\n" date))
+	(insert (format "Has %s comments. %s likes.\n" amount-comments amount-likes))
+	(insert (format "%s\n\n" text))))))
+  
+
+(defun diaspora-show-message-new-buffer ()
+  "Show this message in new buffer. Load the message, and all its comments, and show it!."
+  (interactive)
+  (let ((id-message (get-text-property (+ 1 
+					  (previous-single-property-change (point) 'diaspora-id-message))
+		     'diaspora-id-message)))
+    
+    (diaspora-get-single-message id-message)))
+
+(defun diaspora-get-single-message (id-message)
+  "Get from the `diaspora-single-message-url' URL the given message by id."
+  (let ((buff (get-buffer-create diaspora-single-message-buffer))
+	(buff-http (diaspora-get-url-entry-stream
+		    (format "%s/%s.json" diaspora-single-message-url id-message))))
+    
+    (with-current-buffer buff-http
+      ;; Delete HTTP header!
+      (diaspora-delete-http-header)
+      )
+    
+    (diaspora-parse-single-message-json buff-http buff)
+    (switch-to-buffer buff)
+    (diaspora-mode)))
+
+(defun diaspora-parse-single-message-json (buff-from buff-to)
+  "Parse JSON format of a single message from buffer \"buff-from\" and return into \"buff-to\""
+  (with-current-buffer buff-from
+    ;; Get the post message parsed from JSON
+    (goto-char (point-min))
+    (let ((lstparsed (cdr (assoc 'posts (json-read)))))
+      (with-current-buffer buff-to
+	;; Clean buffer buff-to and insert message
+	(delete-region (point-min) (point-max))
+	(diaspora-show-message lstparsed)
+	)
+      )    
+    ))
 
 (defun diaspora-get-entry-stream-tag (tag)
   "Get stream of tag. Just an idea... needs working."
