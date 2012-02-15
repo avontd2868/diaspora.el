1734bb634c9f163951e301899513ac6ccf5472dc
Tiago Charters Azevedo <tca@diale.org>
2012-02-15 10:20:53 +0000

    Changed the license... <http://unlicense.org/>

--- diaspora.el
+++ diaspora.el
@@ -7,32 +7,43 @@
 ;; Keywords: diaspora*
 ;; URL: http://diale.org/diaspora.html
 
-;; Copyright (c) 2012 Tiago Charters de Azevedo, Christian Giménez
-;;
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-;; 02110-1301, USA.
+;; This is free and unencumbered software released into the public domain.
+
+;; Anyone is free to copy, modify, publish, use, compile, sell, or
+;; distribute this software, either in source code form or as a compiled
+;; binary, for any purpose, commercial or non-commercial, and by any
+;; means.
+
+;; In jurisdictions that recognize copyright laws, the author or authors
+;; of this software dedicate any and all copyright interest in the
+;; software to the public domain. We make this dedication for the benefit
+;; of the public at large and to the detriment of our heirs and
+;; successors. We intend this dedication to be an overt act of
+;; relinquishment in perpetuity of all present and future rights to this
+;; software under copyright law.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;; IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+;; OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+;; ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;; OTHER DEALINGS IN THE SOFTWARE.
+
+;; For more information, please refer to <http://unlicense.org/>
 
 ;;; Commentary:
-
-;; A diaspora* client for emacs
 
 ;; Save all the files in a DIR and add that DIR to `load-path'; 
 ;; for instance `(add-to-list 'load-path "~/emacs.el/disaspora.el/")' to your .emacs
 ;; Files: diaspora.el, diaspora-post.el  and diaspora-stream.el 
 
+; ah, ah...
+
 (setq max-lisp-eval-depth 10000)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (require 'url)
 (require 'url-http)
 (require 'json)
@@ -41,6 +52,7 @@
 (require 'diaspora-new)
 (require 'diaspora-post)
 (require 'diaspora-stream)
+(require 'diaspora-colors)
 (require 'diaspora-notifications)
 
 (defconst diaspora-el-version ".0"
@@ -255,230 +267,6 @@ and  `diaspora-password' has not been setted. `opt' t forces setting."
 					  nil nil))
      (setq diaspora-password (read-passwd "password: ")))))
 
-
-;; Font lock
-
-(defgroup diaspora-faces nil
-  "Faces used in diaspora Mode"
-  :group 'diaspora
-  :group 'faces)
-
-;; (defcustom diaspora-regex-bare-link
-;;   "http://[a-zA-Z0-9-_\./?=&]*"
-;; or "^http://.*"
-;;   "Regular expression for a `http://'"
-;;   :type 'regexp
-;;   :group 'diaspora)
-
-
-;  "[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}T[0-9]\\{2\\}:[0-9]\\{2\\}:[0-9]\\{2\\}Z"
-(defcustom diaspora-regexp-date
-  "[0-9-:T]+Z"
-  "Regular expression date in diaspora stream."
-  :type 'regexp
-  :group 'diaspora)
-
-(defcustom diaspora-regexp-youtube-link
-  "\\(http.*://www.youtube.com/watch\\?v=\\)\\([^\)].*\\)"
-  "Regular expression for a youtube link"
-  :type 'regexp
-  :group 'diaspora)
-
-(defcustom diaspora-regexp-image-alist
-  "\\(`?http.://\\|\\[\\[\\|<\\|`\\)?\\([-+./_0-9a-zA-Z]+\\.\\(GIF\\|JP\\(?:E?G\\)\\|P\\(?:BM\\|GM\\|N[GM]\\|PM\\)\\|SVG\\|TIFF?\\|X\\(?:[BP]M\\)\\|gif\\|jp\\(?:e?g\\)\\|p\\(?:bm\\|gm\\|n[gm]\\|pm\\)\\|svg\\|tiff?\\|x\\(?:[bp]m\\)\\)\\)\\(\\]\\]\\|>\\|'\\)?"
-  "Taken from iimage-mode."
-  :type 'regexp
-  :group 'diaspora)
-
-(defcustom diaspora-regexp-image
-"!\\(\\[[^]]*?\\]\\)(\\(`?http.*:[^\\)?]*\\))"
-  "Regular expression for a [text](file) or an image link ![text](file).
-Note: this is not correct! Needs more thought to get all images right."
-  :type 'regexp
-  :group 'diaspora)
-
-;;"^[a-zA-Z0-9_úùüãâáàéíìõóòñ\s-\.\*\/@]*[a-zA-Z0-9_úùüãâáàéíìõóòñ\s-\.\*\/@]*@[a-zA-Z0-9\s-]*[\.a-zA-Z0-9\s-]*)"
-(defcustom diaspora-regexp-user-entry 
-"^.+ (.+)"
-  "Regular expression for user entry."
-  :type 'regexp
-  :group 'diaspora)
-
-(defcustom diaspora-regexp-tag
-  "#[a-zA-Z0-9_/\.-]+"
-  "Regular expression for a tag."
-  :type 'regexp
-  :group 'diaspora)
-
-(defcustom diaspora-regexp-header-1
-  "^\\(# \\)\\(.*?\\)\\($\\| #+$\\)"
-  "Regular expression for level 1"
-  :type 'regexp
-  :group 'diaspora)
-
-(defcustom diaspora-regexp-header-2
-  "^\\(## \\)\\(.*?\\)\\($\\| #+$\\)"
-  "Regular expression for level 2"
-  :type 'regexp
-  :group 'diaspora)
-
-(defcustom diaspora-regexp-header-3
-  "^\\(### \\)\\(.*?\\)\\($\\| #+$\\)"
-  "Regular expression for level 3"
-  :type 'regexp
-  :group 'diaspora)
-
-
-(defcustom diaspora-regexp-header-4
-  "^\\(#### \\)\\(.*?\\)\\($\\| #+$\\)"
-  "Regular expression for level 4"
-  :type 'regexp
-  :group 'diaspora)
-
-(defconst diaspora-regexp-code
-  "\\(^\\|[^\\]\\)\\(\\(`\\{1,2\\}\\)\\([^ \\]\\|[^ ]\\(.\\|\n[^\n]\\)*?[^ \\]\\)\\3\\)"
-  "Regular expression for matching inline code fragments.")
-
-
-(defconst diaspora-regexp-bold
-  "\\(^\\|[^\\]\\)\\(\\([*_]\\{2\\}\\)\\(.\\|\n[^\n]\\)*?[^\\ ]\\3\\)"
-  "Regular expression for matching bold text.")
-
-(defconst diaspora-regexp-emph
-  "\\(^\\|[^\\]\\)\\(\\([*_]\\)\\([^ \\]\\3\\|[^ ]\\(.\\|\n[^\n]\\)*?[^\\ ]\\3\\)\\)"
-  "Regular expression for matching emph text.")
-
-(defconst diaspora-regexp-email
-  "<\\(\\sw\\|\\s_\\|\\s.\\)+@\\(\\sw\\|\\s_\\|\\s.\\)+>"
-  "Regular expression for matching inline email addresses.")
-
-(defconst diaspora-regexp-blockquote
-  "^>.*$"
-  "Regular expression for matching blockquote lines.")
-
-(defconst diaspora-regexp-hr
-  "^\\(\\*[ ]?\\*[ ]?\\*[ ]?[\\* ]*\\|-[ ]?-[ ]?-[--- ]*\\)$"
-  "Regular expression for matching markdown horizontal rules.")
-
-(defvar diaspora-header-face-1 'diaspora-header-face-1
-  "Face name to use for level-1 headers.")
-
-(defvar diaspora-header-face-2 'diaspora-header-face-2
-  "Face name to use for level-2 headers.")
-
-(defvar diaspora-header-face-3 'diaspora-header-face-3
-  "Face name to use for level-3 headers.")
-
-(defvar diaspora-header-face-4 'diaspora-header-face-4
-  "Face name to use for level-4 headers.")
-
-(defvar diaspora-url-face 'diaspora-url-face
-  "Face name to use for URLs.")
-
-(defvar diaspora-link-face 'diaspora-link-face
-  "Face name to use for links.")
-
-(defvar diaspora-emph-face 'diaspora-emph-face
-  "Face name to use for links.")
-
-(defvar diaspora-bold-face 'diaspora-bold-face
-  "Face name to use for links.")
-
-(defvar diaspora-emph-face 'diaspora-emph-face 
-  "Face name to use for links.")
-
-(defvar diaspora-inline-code-face 'diaspora-inline-code-face
-  "Face name to use for inline code.")
-
-(defvar diaspora-blockquote-face 'diaspora-blockquote-face
-  "Face name to use for blockquote text.")
-
-(defface diaspora-inline-code-face
-  '((t :inherit font-lock-constant-face))
-  "Face for inline code."
-  :group 'diaspora-faces)
-
-(defface diaspora-blockquote-face
-  '((t :inherit font-lock-doc-face))
-  "Face for blockquote sections."
-  :group 'diaspora-faces)
-
-(defface diaspora-header-face
-  '((t :inherit font-lock-function-name-face :weight bold))
-  "Base face for headers."
-  :group 'diaspora-faces)
-
-(defface diaspora-header-face-1
-  '((t :inherit diaspora-header-face))
-  "Face for level-1 headers."
-  :group 'diaspora-faces)
-
-(defface diaspora-header-face-2
-  '((t :inherit diaspora-header-face))
-  "Face for level-2 headers."
-  :group 'diaspora-faces)
-
-(defface diaspora-header-face-3
-  '((t :inherit diaspora-header-face))
-  "Face for level-3 headers."
-  :group 'diaspora-faces)
-
-(defface diaspora-header-face-4
-  '((t :inherit diaspora-header-face))
-  "Face for level-4 headers."
-  :group 'diaspora-faces)
-
-(defface diaspora-link-face
-  '((t :inherit font-lock-keyword-face))
-  "Face for links."
-  :group 'diaspora-faces)
-
-(defface diaspora-url-face
-  '((t :inherit font-lock-string-face))
-  "Face for URLs."
-  :group 'diaspora-faces)
-
-(defface diaspora-emph-face
-  '((t :inherit font-lock-variable-name-face :italic t))
-  "Face for italic text."
-  :group 'diaspora-faces)
-
-(defface diaspora-bold-face
-  '((t :inherit font-lock-variable-name-face :bold t))
-  "Face for bold text."
-  :group 'diaspora-faces)
-
-(defface diaspora-link-face
-  '((t :inherit font-lock-keyword-face))
-  "Face for links."
-  :group 'diaspora-faces)
-
-(defface diaspora-date-face
-  '((t :inherit font-lock-variable-name-face :italic t))
-  "Face for date."
-  :group 'diaspora-faces)
-
-
-(defvar diaspora-mode-font-lock-keywords
-  (list
-;   (cons diaspora-regexp-bare-link '(2 diaspora-url-face t))
-;   (cons diaspora-regexp-date 'diaspora-date-face)
-   (cons diaspora-regexp-blockquote 'diaspora-blockquote-face)
-   (cons diaspora-regexp-user-entry 'diaspora-header-face-1)
-   (cons diaspora-regexp-header-1 'diaspora-header-face-1)
-   (cons diaspora-regexp-header-2 'diaspora-header-face-2)
-   (cons diaspora-regexp-header-3 'diaspora-header-face-3)
-   (cons diaspora-regexp-header-4 'diaspora-header-face-4)
-   (cons diaspora-regexp-hr 'diaspora-header-face-1)
-   (cons diaspora-regexp-image
-         '((1 diaspora-link-face t)
-           (2 diaspora-url-face t)))
-   (cons diaspora-regexp-bold '(2 diaspora-bold-face))
-   (cons diaspora-regexp-emph '(2 diaspora-emph-face))
-   (cons diaspora-regexp-code '(2 diaspora-inline-code-face))
-   (cons diaspora-regexp-email 'diaspora-link-face)
-   (cons diaspora-regexp-tag 'diaspora-url-face))
-  "Syntax highlighting for diaspora files.")
 
 
 
